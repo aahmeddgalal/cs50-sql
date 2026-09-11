e13491b5a747c754df40d9cba90c3895da15063a
Galileo <aahmeddgalal@gmail.com>
2026-09-11 23:45:38 +0300

    cs50-sql

---
+++ README.md
@@ -0,0 +1,2 @@
+# SQL
+#### This is a repo where I practice SQL in the guide of CS50 course
---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "riders" (
+    "id" INTEGER,
+    "name" TEXT
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "stations" (
+    "id" INTEGER,
+    "name" TEXT,
+    "line" TEXT,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "visits" (
+    "rider_id" INTEGER,
+    "station_id" INTEGER
+    FOREIGN KEY("rider_id") REFERENCES "riders"("id"),
+    FOREIGN KEY("station_id") REFERENCES "stations"("id") 
+);
